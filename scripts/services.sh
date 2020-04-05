a275e713a156a6df9bab129c9d93dfc969164512
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 01:30:24 -0400

    Remove dev branch switch

--- scripts/services.sh
+++ scripts/services.sh
@@ -4,9 +4,6 @@ set -e
 # server or client
 MODE=$1
 
-# Switch to dev branch
-cd keel && git checkout dev && cd ~
-
 # Create service definitions
 sudo cp ~/keel/${MODE}/nomad/${MODE}.service /etc/systemd/system/nomad-${MODE}.service
 sudo cp ~/keel/${MODE}/consul/${MODE}.service /etc/systemd/system/consul-${MODE}.service
