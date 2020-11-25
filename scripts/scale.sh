5e750d17b70f8c1cfaa9e3cf0ec3369aac40387e
Brendan Kennedy <brenwken@gmail.com>
2020-11-24 20:39:10 -0500

    Simplify tidal worker tags

--- scripts/scale.sh
+++ scripts/scale.sh
@@ -20,7 +20,7 @@ for (( i=0; i<$NUMBER_OF_INSTANCES; ++i)); do
   --size $INSTANCE_SIZE \
   --enable-private-networking \
   --user-data-file ./start.sh \
-  --tag-names "worker,vpc,ssh,tidal" \
+  --tag-names "ssh,vpc,fabio" \
   --vpc-uuid 88047a46-8be0-4d10-8d25-d5a683505fd6 \
   --ssh-keys 26:e4:37:18:61:e9:29:76:20:68:e1:7f:eb:49:07:2e \
   --ssh-keys a9:da:f0:ba:e6:95:4a:4a:2a:ae:dd:e6:60:5b:bb:10
