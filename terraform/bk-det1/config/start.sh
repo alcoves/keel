5af08e0558291c3607622cb006a4d12080144daf
Brendan Kennedy <brenwken@gmail.com>
2021-03-10 21:32:42 -0500

    Use start scripts

--- terraform/bk-det1/config/start.sh
+++ terraform/bk-det1/config/start.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+TYPE=$1
+PRIVATE_IP=$2
+
+if [ -z "$TYPE" ]; then
+  echo "first argument must be either worker or server"
+  exit 1
+fi
+
+if [ -z "$PRIVATE_IP" ]; then
+  echo "second argument must be a private ip"
+  exit 1
+fi
+
 echo "resyncing repos"
 cd /home/ubuntu/keel && git pull -r
 cd /home/ubuntu/tidal && git pull -r
 
 echo "recompiling tidal"
-go build main.go && cd /home/ubuntu
+cd /home/ubuntu/tidal && make install && cd ~
 
 echo "creating consul/nomad dirs"
 sudo mkdir -p /var/lib/consul
-sudo mkdir -p /var/lib/nomad
+sudo mkdir -p /etc/consul.d
 
-sudo cp /home/ubuntu/consul.service /etc/systemd/system/consul.service
+sudo mkdir -p /var/lib/nomad
+sudo mkdir -p /etc/nomad.d
+
+sudo cp /home/ubuntu/keel/terraform/bk-det1/config/consul.service /etc/systemd/system/consul.service
+sed -i "s/{PRIVATE_IP}/${PRIVATE_IP}/g" /home/ubuntu/keel/terraform/bk-det1/config/${TYPE}/consul.hcl > /etc/consul.d/consul.hcl
 sudo systemctl enable consul.service
+sudo systemctl stop consul.service
 sudo systemctl start consul.service
 
-sudo cp /home/ubuntu/nomad.service /etc/systemd/system/nomad.service
+sudo cp /home/ubuntu/keel/terraform/bk-det1/config/nomad.service /etc/systemd/system/nomad.service
+sed -i "s/{PRIVATE_IP}/${PRIVATE_IP}/g" /home/ubuntu/keel/terraform/bk-det1/config/${TYPE}/nomad.hcl > /etc/nomad.d/nomad.hcl
 sudo systemctl enable nomad.service
-sudo systemctl start nomad.service
-
-nomad acl bootstrap
-consul acl bootstrap
+sudo systemctl stop nomad.service
+sudo systemctl start consul.service
--- terraform/bk-det1/config/worker/consul.hcl
+++ terraform/bk-det1/config/worker/consul.hcl
@@ -1,5 +1,5 @@
 datacenter  = "dc1"
 client_addr = "0.0.0.0"
-bind_addr   = "0.0.0.0"
 retry_join  = ["10.0.0.50"]
+bind_addr   = "{PRIVATE_IP}"
 data_dir    = "/var/lib/consul"
